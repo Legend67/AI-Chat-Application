d540525e64bfa0591f53be514e7f9ac3838dd3da
Legend67 <jaswanth0620001@gmail.com>
2025-12-27 13:31:45 +0530

    Fix index and tsconfig files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// import chatRouter from "./routes/chat";
-
-// dotenv.config(); // load env variables
-
-// /* -------------------- EXPRESS APP -------------------- */
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// /* -------------------- ROUTES -------------------- */
-// app.use("/chat", chatRouter);
-
-// /* -------------------- HEALTH CHECK -------------------- */
-// app.get("/", (_req, res) => {
-//   res.send("AI Support Chat Backend is running!");
-// });
-
-// /* -------------------- SERVER -------------------- */
-// const PORT = 3001;
-// app.listen(PORT, () => {
-//   console.log(`Backend running on http://localhost:${PORT}`);
-// });
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
